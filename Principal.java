acd15e4ba21bbf5721f922d0b75f85b78b1fa73b
marlusmarcos <38912986+marlusmarcos@users.noreply.github.com>
2020-11-02 20:04:02 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,15 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
